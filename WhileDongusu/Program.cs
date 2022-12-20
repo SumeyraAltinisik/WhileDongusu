4dd27b5c44210ca429bcbfa3ba7207b6eaee2d6a
SumeyraAltinisik <sfa2023@gmail.com>
2022-12-20 18:58:25 +0300

    Add project files.

---
+++ WhileDongusu/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileDongusu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir sayı giriniz.");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            while (sayi!=0)
+            {
+                Console.WriteLine("Lütfen yeni bir sayı giriniz.");
+                sayi = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("0 girildiği için döngü bitirildi.");
+            Console.ReadKey();
+        }
+    }
+}
